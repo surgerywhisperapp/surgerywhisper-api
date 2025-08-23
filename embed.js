9a61df618c93e67c58ec0d1c532214bd5ddd1b70
surgerywhisperapp <surgerywhisperapp@gmail.com>
2025-08-23 16:32:59 +0200

    embed.js

---
+++ embed.js
@@ -0,0 +1,27 @@
+const OpenAI = require('openai');
+const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const EMB_MODEL = 'text-embedding-3-small'; // 1536 dims, cheap & good
+
+async function embedOne(text) {
+  const res = await client.embeddings.create({
+    model: EMB_MODEL,
+    input: text
+  });
+  return res.data[0].embedding;
+}
+
+// batch with simple concurrency
+async function embedMany(texts, concurrency = 4) {
+  const out = new Array(texts.length);
+  let i = 0;
+  async function worker() {
+    while (i < texts.length) {
+      const idx = i++;
+      out[idx] = await embedOne(texts[idx]);
+    }
+  }
+  await Promise.all(new Array(concurrency).fill(0).map(worker));
+  return out;
+}
+
+module.exports = { embedOne, embedMany };
