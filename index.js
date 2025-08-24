4397f6fe16a3f7ba80ba3ec48729d103ab98c86b
surgerywhisperapp <surgerywhisperapp@gmail.com>
2025-08-24 18:57:52 +0200

    index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const app = express();
 
 // --- Core middleware ---
 app.use(cors());
-app.use(express.json({ limit: '64mb' }));
-app.use(express.urlencoded({ extended: true, limit: '64mb' }));
+app.use(express.json({ limit: '180mb' }));
+app.use(express.urlencoded({ extended: true, limit: '180mb' }));
 
 // --- Health check ---
 app.get('/', (_req, res) => {
